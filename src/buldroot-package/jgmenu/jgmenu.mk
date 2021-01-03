9fa5a264339bfb79816a0845ea2d8f7dc9bc89b7
Alexandre Freire dos Santos <alexxandre.freire@gmail.com>
2021-01-03 01:40:46 +0100

    add: batocera.plus icon to file manager

--- src/buldroot-package/jgmenu/jgmenu.mk
+++ src/buldroot-package/jgmenu/jgmenu.mk
@@ -3,21 +3,21 @@
 # JGMENU
 #
 ################################################################################
-JGMENU_VERSION = v4.2.1
+JGMENU_VERSION = b8adff9af4d825c499ef050c700e056f6a7bddb9
 JGMENU_SITE = $(call github,johanmalm,jgmenu,$(JGMENU_VERSION))
 JGMENU_DEPENDENCIES = host-libxml2
 
 define JGMENU_CONFIGURE_CMDS
-	cd $(@D); PATH="$(HOST_DIR)/bin:$$PATH" ./configure --with-lx --with-pmenu --prefix=$(TARGET_DIR)/usr
+	cd $(@D); PATH="$(HOST_DIR)/bin:$$PATH" ./configure --with-gtktheme --with-pmenu --prefix=$(TARGET_DIR)/usr
 endef
 
 define JGMENU_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC="$(TARGET_CC)" -C  $(@D)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C  $(@D)
 endef
 
 define JGMENU_INSTALL_TARGET_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C  $(@D) install
-    $(SED) "s|$(TARGET_DIR)||g" $(TARGET_DIR)/usr/bin/jgmenu_run
+	$(SED) "s|$(TARGET_DIR)||g" $(TARGET_DIR)/usr/bin/jgmenu_run
 endef
 
 $(eval $(generic-package))
