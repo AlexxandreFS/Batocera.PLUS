f6b3bda81e2f113404a890218e6aa988d586ce0b
Alexandre Freire dos Santos <alexxandre.freire@gmail.com>
2021-01-13 12:01:02 +0100

    add: fmsx libretro

---
+++ src/buldroot-package/libretro-fmsx/libretro-fmsx.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# FMSX
+#
+################################################################################
+# Version.: Commits on Jan 10, 2021
+LIBRETRO_FMSX_VERSION = 5f9df675521f103a4b1c1eeb3f6abe1688bdf3b2
+LIBRETRO_FMSX_SITE = $(call github,libretro,fmsx-libretro,$(LIBRETRO_FMSX_VERSION))
+LIBRETRO_FMSX_LICENSE = Non-commercial
+
+define LIBRETRO_FMSX_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
+        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_CXX)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_FMSX_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/fmsx_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/fmsx_libretro.so
+endef
+
+$(eval $(generic-package))
