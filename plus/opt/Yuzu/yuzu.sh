f6abfd6c708aebf82693ed397b7d1da0103d6fd0
AlexxandreFS <alexxandre.freire@gmail.com>
2021-04-29 08:39:59 +0200

    fix: yuzu, hotkey + start)

--- plus/opt/Yuzu/yuzu.sh
+++ plus/opt/Yuzu/yuzu.sh
@@ -3,21 +3,20 @@
 ### Batocera.PLUS
 ### Alexxandre Freire dos Santos
 ###
+################################################################################
+
+### DIRECTORIES, FILES AND PARAMETERS
 
 HOME_DIR="${HOME}/configs/yuzu"
 SAVE_DIR=/userdata/saves/switch
 BIOS_DIR=/userdata/bios/yuzu
 
+ROM="${1}"
 P1GUID="${2}"
 
-export HOME="${HOME_DIR}"
-export XDG_DATA_HOME="${SAVE_DIR}"
-export QT_QPA_PLATFORM=xcb
+################################################################################
 
-mkdir -p "${HOME_DIR}"
-mkdir -p "${SAVE_DIR}/yuzu"
-
-# Keys
+### NINTENDO SWITCH KEYS
 
 if [ ! -e "${SAVE_DIR}/yuzu/keys" ] && [ -d "${BIOS_DIR}/keys" ]
 then
@@ -25,20 +24,24 @@ then
     ln -s "${BIOS_DIR}/keys" "${SAVE_DIR}/yuzu/keys"
 fi
 
+################################################################################
 
-# Exit game (hotkey + start)
+### EXIT GAME (hotkey + start)
 
 if [ "${P1GUID}" ]
 then
-    BOTOES="$(/opt/Yuzu/getHotkeyStart "${P1GUID}")"
-    BOTAO_HOTKEY="$(echo "${BOTOES}" | cut -d ' ' -f 1)"
-    BOTAO_START="$(echo "${BOTOES}"  | cut -d ' ' -f 2)"
+    BOTOES=$(/opt/Yuzu/getHotkeyStart ${P1GUID})
+    BOTAO_HOTKEY=$(echo "${BOTOES}" | cut -d ' ' -f 1)
+    BOTAO_START=$(echo "${BOTOES}"  | cut -d ' ' -f 2)
 
-    if [ "${BOTAO_HOTKEY}" ] && [ "${BOTAO_START}" ]; then
+    if [ "${BOTAO_HOTKEY}" ] && [ "${BOTAO_START}" ]
+    then
         # Impede que o xjoykill seja encerrado enquanto o jogo está em execução.
-        while : ; do
-            nice -n 20 xjoykill -hotkey "${BOTAO_HOTKEY}" -start "${BOTAO_START}" -kill /opt/Yuzu/killyuzu
-            if ! [ "$(pidof yuzu)" ]; then
+        while :
+        do
+            nice -n 20 xjoykill -hotkey ${BOTAO_HOTKEY} -start ${BOTAO_START} -kill /opt/Yuzu/killyuzu
+            if ! [ "$(pidof yuzu)" ]
+            then
                   break
             fi
             sleep 5
@@ -46,16 +49,36 @@ then
     fi
 fi
 
-# Exec emulator.
+################################################################################
 
-if [ -e "${1}" ]
+### WORKING PATHS 
+
+mkdir -p "${HOME_DIR}"
+mkdir -p "${SAVE_DIR}/yuzu"
+
+export HOME="${HOME_DIR}"
+export XDG_DATA_HOME="${SAVE_DIR}"
+export QT_QPA_PLATFORM=xcb
+
+################################################################################
+
+### EXEC EMULATOR
+
+if [ -e "${ROM}" ]
 then
-    /opt/Yuzu/yuzu -f -g "${1}"
+    /opt/Yuzu/yuzu -f -g "${ROM}"
 else
     # APPS (F1)
     /opt/Yuzu/yuzu
 fi
 
-kill -9 xjoykill
+################################################################################
+
+### CLOSE xjoykill
+
+if [ "$(pidof xjoykill)" ]
+then
+    killall -9 xjoykill
+fi
 
 exit 0
