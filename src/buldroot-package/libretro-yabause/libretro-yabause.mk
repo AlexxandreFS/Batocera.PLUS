4c9614f9a8ee629ffc7ca58d25fcea929de22d61
AlexxandreFS <alexxandre.freire@gmail.com>
2021-08-03 11:03:54 -0300

    update: libretro-yabause

--- src/buldroot-package/libretro-yabause/libretro-yabause.mk
+++ src/buldroot-package/libretro-yabause/libretro-yabause.mk
@@ -3,8 +3,8 @@
 # YABAUSE
 #
 ################################################################################
-# Version.: Commits on Feb 4, 2021
-LIBRETRO_YABAUSE_VERSION = ea5b11804e30c66b2b32aa6deff1da1241f30492
+# Version.: Commits on Jul 28, 2021
+LIBRETRO_YABAUSE_VERSION = 5c2038a2baf906dfaba01ad4da219ca74d613e9f
 LIBRETRO_YABAUSE_SITE = $(call github,libretro,yabause,$(LIBRETRO_YABAUSE_VERSION))
 LIBRETRO_YABAUSE_LICENSE = GPLv2
 
