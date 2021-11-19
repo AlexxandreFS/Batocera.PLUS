ebd4abf57236e852815ac4ff238d0c56b28e2436
Alexandre Freire dos Santos <alexxandre.freire@gmail.com>
2021-11-19 16:51:55 +0000

    fix: p7zip-gui tmpdir

---
+++ src/buldroot-package/wxwidgets/wxwidgets.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# WXWIDGETS
+#
+################################################################################
+
+WXWIDGETS_VERSION = v3.1.0
+WXWIDGETS_SITE = $(call github,wxWidgets,wxWidgets,$(WXWIDGETS_VERSION))
+WXWIDGETS_DEPENDENCIES = zlib libpng jpeg gdk-pixbuf libgtk2
+WXWIDGETS_INSTALL_STAGING = YES
+
+WXWIDGETS_CONF_OPTS += --with-gtk=2 --without-libtiff --disable-mediactrl
+
+define WXWIDGETS_FIXUP_WXWIDGET_CONFIG
+       ln -sf $(STAGING_DIR)/usr/lib/wx/config/*-buildroot-linux-gnu-gtk2-unicode-* $(STAGING_DIR)/usr/bin/wx-config
+	$(SED) 's%^prefix=.*%prefix=$(STAGING_DIR)/usr%' \
+		$(STAGING_DIR)/usr/bin/wx-config
+	$(SED) 's%^exec_prefix=.*%exec_prefix=$${prefix}%' \
+		$(STAGING_DIR)/usr/bin/wx-config
+endef
+
+define WXWIDGETS_REMOVE_WXWIDGETS_CONFIG
+	rm -f $(TARGET_DIR)/usr/bin/wx-config
+	rm -rf $(TARGET_DIR)/usr/lib/wx/config
+endef
+
+WXWIDGETS_POST_INSTALL_TARGET_HOOKS  += WXWIDGETS_REMOVE_WXWIDGETS_CONFIG
+WXWIDGETS_POST_INSTALL_STAGING_HOOKS += WXWIDGETS_FIXUP_WXWIDGET_CONFIG
+
+$(eval $(autotools-package))
