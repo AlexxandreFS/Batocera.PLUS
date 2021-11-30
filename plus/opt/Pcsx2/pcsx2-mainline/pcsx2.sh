b38a82deecd52d8ce966f77a66c5c24081e1ca4c
sabotendarsama <sabotendarsama@gmail.com>
2021-11-30 21:23:59 +0000

    add: pcsx2-mainline implementations

--- plus/opt/Pcsx2/pcsx2-mainline/pcsx2.sh
+++ plus/opt/Pcsx2/pcsx2-mainline/pcsx2.sh
@@ -4,26 +4,28 @@
 ##
 ## Código escrito por: Sérgio de Carvalho Júnior
 ## Supervisão: Alexandre Freire dos Santos
-##
+## https://forums.pcsx2.net/Thread-PCSX2-Widescreen-Game-Patches?pid=271674#pid271674
+## https://www.mediafire.com/file/j48lgoeeli5evve/PCSX_Cheats_Pack_v1.1_-_Mateus_Auri.rar/file
 ################################################################################
 
 ### DIRECTORIES, FILES AND PARAMETERS
 
 ROM="${1}"
-P1GUID="${2}"
-RESOLUTION="${3}"  #videomode
-WIDESCREEN="${4}"  #ratio
-BOOTANIM="${5}"    #fullboot
-I_RES="${6}"       #internal_resolution
-A_FILT="${7}"      #anisotropic_filtering
-VSYNC="${8}"       #vsync
+RESOLUTION="${2}"  #videomode
+WIDESCREEN="${3}"  #ratio
+BOOTANIM="${4}"    #fullboot
+I_RES="${5}"       #internal_resolution
+A_FILT="${6}"      #anisotropic_filtering
+WSCRH="${7}"       #widescreen hack
+PRESET="${8}"      #speed hacks
 
-EMU_DIR='/opt/Pcsx2/pcsx2-mainline'
-SAVE_DIR='/userdata/saves/ps2/pcsx2-mainline'
-
-#echo "${ROM}" "${P1GUID}" "${RESOLUTION}" "${WIDESCREEN}" "${BOOTANIM}" "${I_RES}" "${A_FILT}" "${VSYNC}" > "${HOME}/../PARAMETROS.TXT"
+echo "${ROM}" "${RESOLUTION}" "${WIDESCREEN}" "${BOOTANIM}" "${I_RES}" "${A_FILT}" "${WSCRH}" "${PRESET}" > "${HOME}/../PARAMETROS.TXT"
 
 #exit 0
+
+EMU_DIR='/opt/Pcsx2/pcsx2-mainline'
+SAVE_DIR='/userdata/saves/ps2'
+CONFIG_DIR="${HOME}/configs/pcsx2-mainline"
 
 ### EXPORTS
 
@@ -37,43 +39,75 @@ export GSETTINGS_SCHEMA_DIR="${EMU_DIR}/schemas"
 
 function CreateDirs()
 {
- # Create folders to batocera.linux pcsx2
- mkdir -p "${HOME}/configs/PCSX2/cheats" \
-          "${HOME}/configs/PCSX2/cheats_ws" \
-          "${HOME}/configs/PCSX2/inis" \
-          "${HOME}/configs/PCSX2/logs" \
-          "${HOME}/configs/PCSX2/memcards"
+ # Create folders to batocera.plus pcsx2
+ mkdir -p "${HOME}/logs/pcsx2-mainline"
+ DIRS='memcards cheats cheats_ws sstates-mainline custom-inis-mainline'
+ for i in ${DIRS}; do
+   if [ ! -e "${SAVE_DIR}/${i}" ]; then
+      mkdir -p "${SAVE_DIR}/${i}"
+   fi 
+ done
+}
 
-# Create folders to batocera.plus pcsx2
- mkdir -p "${SAVE_DIR}/inis" \
-          "${SAVE_DIR}/snaps" \
-          "${SAVE_DIR}/sstates"
+function CreateInis()
+{
+ mkdir -p "${CONFIG_DIR}"
+ 
+ touch "${CONFIG_DIR}/PCSX2_ui.ini"
+ echo 'PresetIndex=1'      >> "${CONFIG_DIR}/PCSX2_ui.ini"
+ echo '[Filenames]'        >> "${CONFIG_DIR}/PCSX2_ui.ini"
+ echo 'BIOS=scph39001.bin' >> "${CONFIG_DIR}/PCSX2_ui.ini"
+ echo '[ProgramLog]'       >> "${CONFIG_DIR}/PCSX2_ui.ini"
+ echo 'Visible=disabled'   >> "${CONFIG_DIR}/PCSX2_ui.ini"
+ echo '[GSWindow]'         >> "${CONFIG_DIR}/PCSX2_ui.ini"
+ echo 'AspectRatio=4:3'    >> "${CONFIG_DIR}/PCSX2_ui.ini"
 
- touch "${SAVE_DIR}/inis/PCSX2_ui.ini"
- echo '[Filenames]'        >> "${SAVE_DIR}/inis/PCSX2_ui.ini"
- echo 'BIOS=scph39001.bin' >> "${SAVE_DIR}/inis/PCSX2_ui.ini"
- echo '[ProgramLog]'       >> "${SAVE_DIR}/inis/PCSX2_ui.ini"
- echo 'Visible=disabled'   >> "${SAVE_DIR}/inis/PCSX2_ui.ini"
- echo '[GSWindow]'         >> "${SAVE_DIR}/inis/PCSX2_ui.ini"
- echo 'AspectRatio=4:3'    >> "${SAVE_DIR}/inis/PCSX2_ui.ini"
+ touch "${CONFIG_DIR}/GS.ini"
+ echo 'vsync = 0'              >> "${CONFIG_DIR}/GS.ini"
+ echo 'upscale_multiplier = 1' >> "${CONFIG_DIR}/GS.ini"
+ echo 'MaxAnisotropy = 0'      >> "${CONFIG_DIR}/GS.ini"
+ echo 'UserHacks = 0'          >> "${CONFIG_DIR}/GS.ini"
 
- touch "${SAVE_DIR}/inis/GS.ini"
- echo 'vsync = 0'              >> "${SAVE_DIR}/inis/GS.ini"
- echo 'upscale_multiplier = 1' >> "${SAVE_DIR}/inis/GS.ini"
- echo 'MaxAnisotropy = 0'      >> "${SAVE_DIR}/inis/GS.ini"
+ touch "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo '[EmuCore]'                        >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'EnableWideScreenPatches=disabled' >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo '[EmuCore/Speedhacks]'             >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'EECycleRate=0'                    >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'EECycleSkip=0'                    >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'fastCDVD=disabled'                >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'IntcStat=enabled'                 >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'WaitLoop=enabled'                 >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'vuFlagHack=enabled'               >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'vuThread=disabled'                >> "${CONFIG_DIR}/PCSX2_vm.ini"
+ echo 'vu1Instant=enabled'               >> "${CONFIG_DIR}/PCSX2_vm.ini"
+
+ touch "${CONFIG_DIR}/PAD.ini"
+ echo 'first_time_wizard = 0'   >> "${CONFIG_DIR}/PAD.ini"
+ echo 'options = 1'             >> "${CONFIG_DIR}/PAD.ini"
 }
 
 ################################################################################
 
-if [ ! "$(ls -A "${SAVE_DIR}" 2> /dev/null)" ] || [ ! "$(ls -A "${HOME}/configs/PCSX2"  2> /dev/null)" ]; then
+if [ ! "$(ls -A "${HOME}/configs/pcsx2-mainline"  2> /dev/null)" ]; then
+   CreateInis
+elif [ ! "$(ls -A "${SAVE_DIR}/custom-inis-mainline" 2> /dev/null)" ]; then
    CreateDirs
 fi
 
 ################################################################################
 
-# per-game config
+### PERGAME CONFIG
 
-mkdir -p "${SAVE_DIR}/custon-configs/$(basename "${ROM%.*}" )"
+if [ "${CUSTOM}" == '1' ]; then
+   if [ ! "$(ls -A "${HOME}/configs/pcsx2-mainline"  2> /dev/null)" ]; then
+      CreateDirs
+      CreateInis
+   fi
+   GAME_NAME="${SAVE_DIR}/custom-inis-mainline/$(basename "${ROM%.*}" )"
+   mkdir -p "${SAVE_DIR}/custom-inis-mainline/$(basename "${ROM%.*}" )"
+   cp -r -f "${HOME}/configs/pcsx2-mainline/"* "${SAVE_DIR}/custom-inis-mainline/${GAME_NAME}"
+   INIS="--cfgpath ${SAVE_DIR}/custom-inis-mainline/${GAME_NAME}"
+fi
 
 ################################################################################
 
@@ -89,9 +123,9 @@ fi
 
 case "${WIDESCREEN}" in
     4/3|1/1|16/15|3/2|3/4|4/4|5/4|6/5|7/9|8/7|auto|custom|squarepixel)
-        sed -i s/'^AspectRatio=.*/AspectRatio=4:3/'  "${SAVE_DIR}/inis/PCSX2_ui.ini" ;;
+        sed -i s/'^AspectRatio=.*/AspectRatio=4:3/'  "${CONFIG_DIR}/PCSX2_ui.ini" ;;
     16/9|19/12|19/14|2/1|21/9|30/17|32/9|4/1|8/3)
-        sed -i s/'^AspectRatio=.*/AspectRatio=16:9/' "${SAVE_DIR}/inis/PCSX2_ui.ini" ;;
+        sed -i s/'^AspectRatio=.*/AspectRatio=16:9/' "${CONFIG_DIR}/PCSX2_ui.ini" ;;
 esac
 
 ################################################################################
@@ -99,9 +133,9 @@ esac
 ### VSYNC
 
 if [ "${VSYNC}" == '1' ] || [ "${VSYNC}" == 'auto' ]; then
-   sed -i s/'^vsync =.*/vsync = 1 /' "${SAVE_DIR}/inis/GS.ini"
+   sed -i s/'^vsync =.*/vsync = 1/' "${CONFIG_DIR}/GS.ini"
 else
-   sed -i s/'^vsync =.*/vsync = 0 /' "${SAVE_DIR}/inis/GS.ini"
+   sed -i s/'^vsync =.*/vsync = 0/' "${CONFIG_DIR}/GS.ini"
 fi
 
 ################################################################################
@@ -109,9 +143,19 @@ fi
 ### INTERNAL RESOLUTION
 
 if [ "${I_RES}" != 'auto' ]; then
-   sed -i s/'^upscale_multiplier =.*/upscale_multiplier = '"${I_RES}"'/' "${SAVE_DIR}/inis/GS.ini"
+   sed -i s/'^upscale_multiplier =.*/upscale_multiplier = '"${I_RES}"'/' "${CONFIG_DIR}/GS.ini"
 else
-   sed -i s/'^upscale_multiplier =.*/upscale_multiplier = 1 /' "${SAVE_DIR}/inis/GS.ini"
+   sed -i s/'^upscale_multiplier =.*/upscale_multiplier = 1/' "${CONFIG_DIR}/GS.ini"
+fi
+
+################################################################################
+
+### WIDE SCREEN HACK
+
+if [ "${WSCRH}" == '0' ] || [ "${WSCRH}" == 'auto' ]; then
+   sed -i s/'^EnableWideScreenPatches=.*/EnableWideScreenPatches=disabled/' "${CONFIG_DIR}/PCSX2_vm.ini"
+else
+   sed -i s/'^EnableWideScreenPatches=.*/EnableWideScreenPatches=enabled/' "${CONFIG_DIR}/PCSX2_vm.ini"
 fi
 
 ################################################################################
@@ -119,16 +163,74 @@ fi
 ### ANISOTROPIC FILTERING
 
 if [ "${A_FILT}" != 'auto' ]; then
-   sed -i s/'^MaxAnisotropy =.*/MaxAnisotropy = '"${A_FILT}"'/' "${SAVE_DIR}/inis/GS.ini"
+   sed -i s/'^MaxAnisotropy =.*/MaxAnisotropy = '"${A_FILT}"'/' "${CONFIG_DIR}/GS.ini"
 else
-   sed -i s/'^MaxAnisotropy =.*/MaxAnisotropy = 0 /' "${SAVE_DIR}/inis/GS.ini"
+   sed -i s/'^MaxAnisotropy =.*/MaxAnisotropy = 0/' "${CONFIG_DIR}/GS.ini"
 fi
 
 ################################################################################
 
-### RUN
+### SPEED HACKS
 
-# $EMU_DIR/bin/PCSX2
+case ${PRESET} in
+   safest)
+      sed -i s/'^EnablePresets=.*/EnablePresets=enabled/' "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'^PresetIndex=.*/PresetIndex=0/'           "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'^vsync =.*/vsync = 1/'                    "${CONFIG_DIR}/GS.ini"
+      ;;
+   default|auto)
+      sed -i s/'^EnablePresets=.*/EnablePresets=enabled/' "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'^PresetIndex=.*/PresetIndex=1/'           "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'^vsync =.*/vsync = 1/'                    "${CONFIG_DIR}/GS.ini"
+      ;;
+   balanced)
+      sed -i s/'^EnablePresets=.*/EnablePresets=enabled/' "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'^PresetIndex=.*/PresetIndex=2/'           "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'^vsync =.*/vsync = 1/'                    "${CONFIG_DIR}/GS.ini"
+      ;;
+   aggressive)
+      sed -i s/'^EnablePresets=.*/EnablePresets=disabled/' "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'UserHacks =.*/UserHacks = 1/'              "${CONFIG_DIR}/GS.ini"
+      sed -i s/'^vsync =.*/vsync = 0/'                     "${CONFIG_DIR}/GS.ini"
+      sed -i s/'^EECycleRate=.*/EECycleRate=-1/'           "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^EECycleSkip=.*/EECycleSkip=0/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^fastCDVD=.*/fastCDVD=disabled/'           "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^IntcStat=.*/IntcStat=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^WaitLoop=.*/WaitLoop=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vuFlagHack=.*/vuFlagHack=enabled/'        "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vuThread=.*/vuThread=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vu1Instant=.*/vu1Instant=enabled/'        "${CONFIG_DIR}/PCSX2_vm.ini"
+      ;;
+   vaggressive)
+      sed -i s/'^EnablePresets=.*/EnablePresets=disabled/' "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'UserHacks =.*/UserHacks = 1/'              "${CONFIG_DIR}/GS.ini"
+      sed -i s/'^vsync =.*/vsync = 0/'                     "${CONFIG_DIR}/GS.ini"
+      sed -i s/'^EECycleRate=.*/EECycleRate=-2/'           "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^EECycleSkip=.*/EECycleSkip=0/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^fastCDVD=.*/fastCDVD=disabled/'           "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^IntcStat=.*/IntcStat=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^WaitLoop=.*/WaitLoop=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vuFlagHack=.*/vuFlagHack=enabled/'        "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vuThread=.*/vuThread=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vu1Instant=.*/vu1Instant=enabled/'        "${CONFIG_DIR}/PCSX2_vm.ini"
+      ;;
+   mharmful)
+      sed -i s/'^EnablePresets=.*/EnablePresets=disabled/' "${CONFIG_DIR}/PCSX2_ui.ini"
+      sed -i s/'UserHacks =.*/UserHacks = 1/'              "${CONFIG_DIR}/GS.ini"
+      sed -i s/'^vsync =.*/vsync = 0/'                     "${CONFIG_DIR}/GS.ini"
+      sed -i s/'^EECycleRate=.*/EECycleRate=1/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^EECycleSkip=.*/EECycleSkip=1/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^fastCDVD=.*/fastCDVD=disabled/'           "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^IntcStat=.*/IntcStat=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^WaitLoop=.*/WaitLoop=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vuFlagHack=.*/vuFlagHack=enabled/'        "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vuThread=.*/vuThread=enabled/'            "${CONFIG_DIR}/PCSX2_vm.ini"
+      sed -i s/'^vu1Instant=.*/vu1Instant=enabled/'        "${CONFIG_DIR}/PCSX2_vm.ini"
+      ;;
+esac
+
+################################################################################
+### RUN
 
 if [ -e "${ROM}" ]; then
    $MANGOHUD_CMD $EMU_DIR/PCSX2 --fullscreen --nogui $FULLBOOT "${ROM}"
--- plus/opt/Pcsx2/pcsx2.sh
+++ plus/opt/Pcsx2/pcsx2.sh
@@ -8,7 +8,62 @@ readonly CORE="${1}"
 readonly P1GUID="${2}"
 readonly ROM="${3}"
 
+RESOLUTION="${4}"  #videomode
+WIDESCREEN="${5}"  #ratio
+BOOTANIM="${6}"    #fullboot
+I_RES="${7}"       #internal_resolution
+A_FILT="${8}"      #anisotropic_filtering
+WSCRH="${9}"       #widescreen hack
+SPEEDHACKS="${10}" #speedhacks
+
 readonly PCSX2_DIR="$(dirname ${0})"
+
+################################################################################
+
+function TextLocalization()
+{
+   LANG="$(batocera-settings -command load -key system.language)"
+   case $LANG in
+      pt_BR) MSG[1]='\n ESCOLHA UMA OPÇÃO. \n' ;;
+      es_ES) MSG[1]='\n ESCOGE UNA OPCIÓN. \n' ;;
+      *)     MSG[1]='\n CHOOSE A OPTION. \n'
+   esac
+}
+
+function choseEmu()
+{
+   yad --form \
+   --title='PCSX2 CONFIGURATOR' \
+   --window-icon='/usr/share/icons/batocera/pcsx2.png' \
+   --text=''"${MSG[1]}"'' \
+   --text-align=center \
+   --button='PCSX2:0' \
+   --button='PCSX2-LEGACY:1' \
+   --button='PCSX2-MAINLINE:2' \
+   --fixed \
+   --center \
+   --close-on-unfocus
+
+   case ${?} in
+      0) GUI='0' ;;
+      1) GUI='1' ;;
+      2) GUI='2' ;;
+      *) exit 0
+   esac
+}
+
+################################################################################
+
+if [ ! -e "${ROM}" ]; then
+    TextLocalization
+    choseEmu
+    case ${GUI} in
+        0) /usr/bin/batocera-config-pcsx2 ;;
+        1) /opt/Pcsx2/pcsx2-legacy/pcsx2.sh ;;
+        2) /opt/Pcsx2/pcsx2-mainline/pcsx2.sh ;;
+    esac
+fi
+
 
 ################################################################################
 
@@ -56,9 +111,11 @@ function killXjoyKill()
 
 exitHotkeyStart
 
-if [ "${CORE}" == 'pcsx-mainline' ]
+if [ "${CORE}" == 'pcsx2-mainline' ]
 then
-    /opt/Pcsx2/pcsx2-mainline/pcsx2.sh "${ROM}"
+    /opt/Pcsx2/pcsx2-mainline/pcsx2.sh \
+            "${ROM}" "${P1GUID}" "${RESOLUTION}" "${WIDESCREEN}" "${BOOTANIM}" "${I_RES}" \
+            "${A_FILT}" "${WSCRH}" "${SPEEDHACKS}"
 elif [ "${CORE}" == 'pcsx2-legacy' ]
 then
     /opt/Pcsx2/pcsx2-legacy/pcsx2.sh "${ROM}"
