57ecfeeab4a27a8e2f415456d008fad798b8a362
sabotendarsama <sabotendarsama@gmail.com>
2022-04-10 11:19:53 +0000

    add: pcsx2-legacy custom ability try 2

--- plus/opt/Pcsx2/pcsx2-mainline/pcsx2-mainline.sh
+++ plus/opt/Pcsx2/pcsx2-mainline/pcsx2-mainline.sh
@@ -111,7 +111,7 @@ fi
 
 if [ -e "${ROM}" ] && [ "${CUSTOM}" == '1' ]; then
     ${EMU_DIR}/PCSX2 --nogui --fullscreen --cfgpath="${CONFIG_DIR}" \
-          ${FULLBOOT} "${ROM}" > ${HOME}/logs/pcsx2-mainline.log 2>&1
+        ${FULLBOOT} "${ROM}" > ${HOME}/logs/pcsx2-mainline.log 2>&1
 elif [ -e "${ROM}" ] && [ "${CUSTOM}" == '2' ]; then
     ${EMU_DIR}/PCSX2 --cfgpath="${CONFIG_DIR}" \
         "${ROM}" > ${HOME}/logs/pcsx2-mainline.log 2>&1
