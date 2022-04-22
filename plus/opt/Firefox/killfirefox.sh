65d63ea0f9f26d3c1c7a231b091ae62c09f1d9ce
sabotendarsama <sabotendarsama@gmail.com>
2022-04-22 17:04:54 +0000

    revert: firefox kill script

--- plus/opt/Firefox/killfirefox.sh
+++ plus/opt/Firefox/killfirefox.sh
@@ -6,15 +6,25 @@
 ## Encerra a execução do firefox.
 ## 
 
-if pidof -s firefox > /dev/null
-then
-    killall -15 firefox
-fi 
+FIREFOX_PID="$(pidof -s firefox)"
 
-if pidof -s firefox > /dev/null
+if [ -z "${FIREFOX_PID}" ]
 then
-    killall -15 firefox
-fi 
+    exit 0
+fi
+
+kill -15 ${FIREFOX_PID}
+sleep 0.2
+
+FIREFOX_PID="$(pidof -s firefox)"
+
+if [ -z "${FIREFOX_PID}" ]
+then
+    echo 'Firefox closed!'
+else
+    sleep 0.5
+    kill -9 ${FIREFOX_PID}
+    echo 'Firefox is dead!'
+fi
 
 exit 0
-
