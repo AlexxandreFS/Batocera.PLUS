7051df015db8d8adcf74997cbf0baffca87e466b
sabotendarsama <sabotendarsama@gmail.com>
2022-04-22 16:59:38 +0000

    revert: pcsx2 kill script

--- plus/opt/Pcsx2/killpcsx2.sh
+++ plus/opt/Pcsx2/killpcsx2.sh
@@ -6,15 +6,25 @@
 ## Encerra a execução do pcsx2.
 ## 
 
-if pidof -s PCSX2 > /dev/null
-then
-    killall -15 PCSX2
-fi 
+PCSX2_PID="$(pidof -s PCSX2)"
 
-if pidof -s PCSX2 > /dev/null
+if [ -z "${PCSX2_PID}" ]
 then
-    killall -15 PCSX2
-fi 
+    exit 0
+fi
+
+kill -15 ${PCSX2_PID}
+sleep 0.2
+
+PCSX2_PID="$(pidof -s PCSX2)"
+
+if [ -z "${PCSX2_PID}" ]
+then
+    echo 'PCSX2 closed!'
+else
+    sleep 0.5
+    kill -9 ${PCSX2_PID}
+    echo 'PCSX2 is dead!'
+fi
 
 exit 0
-
