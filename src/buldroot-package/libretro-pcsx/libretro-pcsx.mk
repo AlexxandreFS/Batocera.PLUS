e6acdd45588e952c470489a69b302c0bf323e83f
AlexxandreFS <alexxandre.freire@gmail.com>
2022-11-19 02:34:57 +0000

    update: pcsx-rearmed src

---
+++ src/buldroot-package/libretro-pcsx/libretro-pcsx.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# PCSXREARMED
+#
+################################################################################
+# Version.: Commits on Nov 14, 2022
+LIBRETRO_PCSX_VERSION = a4e249a1373cf6269e1e4e0d60105e72210e67d3
+LIBRETRO_PCSX_SITE = $(call github,libretro,pcsx_rearmed,$(LIBRETRO_PCSX_VERSION))
+LIBRETRO_PCSX_LICENSE = GPLv2
+
+LIBRETRO_PCSX_PLATFORM = $(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ODROIDN2),y)
+	LIBRETRO_PCSX_PLATFORM = CortexA73_G12B
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_LIBRETECH_H5),y)
+	LIBRETRO_PCSX_PLATFORM = h5
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
+	LIBRETRO_PCSX_PLATFORM = rpi3
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ODROIDGOA),y)
+	LIBRETRO_PCSX_PLATFORM = classic_armv8_a35
+endif
+
+define LIBRETRO_PCSX_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PCSX_PLATFORM)" CFLAGS_GPU_NEON=-msse4 DYNAREC=1
+endef
+
+define LIBRETRO_PCSX_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/pcsx_rearmed_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/pcsx_rearmed_libretro.so
+endef
+
+$(eval $(generic-package))
