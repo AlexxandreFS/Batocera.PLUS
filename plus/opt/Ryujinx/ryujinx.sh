02780d1c75e9be8f6cd143dd907fa07db69ecdb9
AlexxandreFS <alexxandre.freire@gmail.com>
2023-01-17 23:12:43 +0000

    Revert "fiz: Ryujinx small corrections"
    This reverts commit 0871ce099d2763b8c3d95efb536b46c83d45571d.

--- plus/opt/Ryujinx/ryujinx.sh
+++ plus/opt/Ryujinx/ryujinx.sh
@@ -16,20 +16,17 @@
 ### DIRECTORIES, FILES AND PARAMETERS
 
 ROM="${1}"
-P1GUID="${2}"
-RENDER="${3}"
+RENDER="${2}"
 
 RYUJINX_EMU=/opt/Ryujinx/publish/Ryujinx
 RYUJINX_DIR=/opt/Ryujinx
 SAVE_DIR=/userdata/saves/switch
 BIOS_DIR="${HOME}/../bios/switch"
 
-export LD_LIBRARY_PATH="/opt/Ryujinx/lib:${LD_LIBRARY_PATH}"
 export XDG_CONFIG_HOME="${SAVE_DIR}"
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 export GSETTINGS_SCHEMA_DIR=/opt/Ryujinx/gschemas
 export QT_PLUGIN_PATH=/opt/Ryujinx/plugins
-
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 ################################################################################
 
@@ -40,33 +37,22 @@ function createDirs()
     if ! [ -d "${SAVE_DIR}/Ryujinx" ]; then
         # make config dir
         mkdir -p "${SAVE_DIR}/Ryujinx/system" \  # keys folder
-                 "${SAVE_DIR}/Ryujinx/bis/system/Contents"
+                 "${SAVE_DIR}/Ryujinx/bis/system/Contents/registered"
         cp -f "${RYUJINX_DIR}/default_config/Config.json" "${SAVE_DIR}/Ryujinx"
     fi
 
     # create keys dir
-    if [ -f "${BIOS_DIR}/keys/prod.keys" ] && [ -f "${BIOS_DIR}/keys/title.keys" ]; then
-        rm -rf "${SAVE_DIR}/Ryujinx/system/"*
+	if [ -f "${BIOS_DIR}/keys/prod.keys" ] && [ -f "${BIOS_DIR}/keys/title.keys" ]; then
+	    rm -r "${SAVE_DIR}/Ryujinx/system/"* 2> /dev/null
         ln -sf "${BIOS_DIR}/keys/"*  "${SAVE_DIR}/Ryujinx/system"
     else
-        rm -rf "${SAVE_DIR}/Ryujinx/system/"*
-    fi
-
-    # create firmware dir
-    if [ ! -d "${SAVE_DIR}/Ryujinx/bis/system/Contents/registered" ]; then # frist execution
-        mkdir -r "${BIOS_DIR}/firmware"
-        ln -s    "${BIOS_DIR}/firmware" "${SAVE_DIR}/Ryujinx/bis/system/Contents/registered"
-    fi
-
-    # make log dir
-    if ! [ -d "${HOME}/logs/ryujinx" ]; then
-        mkdir -p "${HOME}/logs/ryujinx"
+	    rm -r "${SAVE_DIR}/Ryujinx/system/"* 2> /dev/null
     fi
 }
 
 ################################################################################
 
-### FIRMWARE CHECK
+### Firmware check
 
 function TextLocalization()
 {
@@ -75,23 +61,29 @@ function TextLocalization()
       es_ES) MSG[1]='\n NO SE HA INSTALADO FIRMWARE, INSTALAR ANTES DE EJECUTAR UN JUEGO. \n' ;;
       *)     MSG[1]='\n NO FIRMWARE HAS BEEN INSTALLED, INSTALL BEFORE RUN A GAME. \n'
    esac
+
+   if [ ${LANG%.*} == 'pt_BR' ] || [ ${LANG%.*} == 'es_ES' ]; then
+       sed -i 's/"language_code":.*/"language_code": "'"${LANG%.*}"'",/' "${SAVE_DIR}/Ryujinx/Config.json"
+   else
+       sed -i 's/"language_code":.*/"language_code": "'"${LANG%.*}"'",/' "${SAVE_DIR}/Ryujinx/Config.json"
+   fi
+
 }
 
 function FirmwareWarning()
 {
    yad --form \
-       --title='WARNING' \
-       --window-icon='/usr/share/icons/batocera/ryujinx.png' \
-       --text=''"${MSG[1]}"'' \
-       --undecorated \
-       --text-align=center \
-       --on-top \
-       --fixed \
-       --center \
-       --no-escape \
-       --timeout=3 \
-       --no-buttons &
-
+   --title='WARNING' \
+   --window-icon='/usr/share/icons/batocera/ryujinx.png' \
+   --text=''"${MSG[1]}"'' \
+   --undecorated \
+   --text-align=center \
+   --on-top \
+   --fixed \
+   --center \
+   --no-escape \
+   --timeout=3 \
+   --no-buttons &
    exit 0
 }
 
@@ -102,35 +94,41 @@ fi
 
 ################################################################################
 
-### CHECK DIRS
+### Check dirs
 
 createDirs
 
 ################################################################################
 
+### LOCALIZATION
+
+TextLocalization
+
+################################################################################
+
 if [ "${RENDER}" != 'vulkan' ]; then
-    sed -i 's/"graphics_backend":.*/"graphics_backend": "Vulkan",/' "${SAVE_DIR}/Ryujinx/Config.json"
-else
     sed -i 's/"graphics_backend":.*/"graphics_backend": "OpenGl",/' "${SAVE_DIR}/Ryujinx/Config.json"
+else
+    sed -i 's/"graphics_backend":.*/"graphics_backend": "Vulkan",/' "${SAVE_DIR}/Ryujinx/Config.json"
 fi
 
 ################################################################################
 
-### FIX SOME SETTINGS
+### Fix some settings
 
-sed -i 's/"check_updates_on_start":.*/"check_updates_on_start": false,/'         "${SAVE_DIR}/Ryujinx/Config.json"
-sed -i 's/"enable_discord_integration":.*/"enable_discord_integration": false,/' "${SAVE_DIR}/Ryujinx/Config.json"
-sed -i 's/"show_confirm_exit":.*/"show_confirm_exit": false,/'                   "${SAVE_DIR}/Ryujinx/Config.json"
-sed -i 's/"audio_backend":.*/"audio_backend": "OpenAl",/'                        "${SAVE_DIR}/Ryujinx/Config.json"
+sed -i 's/"check_updates_on_start":.*/"check_updates_on_start": false,/'          "${SAVE_DIR}/Ryujinx/Config.json"
+sed -i 's/"enable_discord_integration":.*/"enable_discord_integration": false,/'  "${SAVE_DIR}/Ryujinx/Config.json"
+sed -i 's/"show_confirm_exit":.*/"show_confirm_exit": false,/'                    "${SAVE_DIR}/Ryujinx/Config.json"
+sed -i 's/"audio_backend":.*/"audio_backend": "OpenAl",/'                         "${SAVE_DIR}/Ryujinx/Config.json"
 
 ################################################################################
 
 ### EXECUTION
 
 if [ -e "${ROM}" ]; then
-   ${MANGOHUD_CMD} ${RYUJINX_EMU} --fullscreen "${ROM}"
+   $MANGOHUD_CMD $RYUJINX_EMU --fullscreen "${ROM}"
 else
-   ${RYUJINX_EMU}
+   $RYUJINX_EMU
 fi
 
 ################################################################################
